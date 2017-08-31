e4205c148544fb063a5a9c0e6119cb73c0b01aba
juagarciaco <juagarciaco@unal.edu.co>
2017-08-31 15:33:53 -0500

    V1.0

--- agracionComposicion/src/agracioncomposicion/Universidad.java
+++ agracionComposicion/src/agracioncomposicion/Universidad.java
@@ -5,10 +5,68 @@
  */
 package agracioncomposicion;
 
+import java.util.*;
+
 /**
  *
  * @author Estudiante
  */
 public class Universidad {
+
+private String nombre;
+private ArrayList<Facultad> facultades;
+private TreeMap<Integer,Estudiante> estudiantes;
+
+    public Universidad(String nombre) {
+        this.nombre = nombre;
+        this.facultades = new ArrayList<>();
+        this.estudiantes = new TreeMap<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
+    public boolean addFacultad (String nombre){
+        Facultad facultad = new Facultad (nombre);
+        if (this.facultades.add(facultad))
+            return true;
+        else
+            return false;
+    }
+    
+    public boolean addEstudiante (Estudiante estudiante){
+        if (this.estudiantes.put(estudiante.getCodigo(),estudiante)!=null)
+            return true;
+        else
+            return false;
+    }
+    
+    public boolean addEstudianteFacultad (String nombreFacultad, Estudiante estudiante){
+        boolean resultado = false;
+        for (int i =0; i< this.facultades.size();i++) {
+            if (this.facultades.get(i).getNombre().equals(nombreFacultad)){
+                this.facultades.get(i).addEstudiante(estudiante);
+                resultado = true;
+                break;
+            }
+        }
+        return resultado;
+    }
+    
+    public ArrayList listAllFacultades (){
+        return this.facultades;
+    }
+    
+    public ArrayList listAllEstudiantes (){
+        return (ArrayList)this.estudiantes.values();
+    }
+
+
+
+
 }
